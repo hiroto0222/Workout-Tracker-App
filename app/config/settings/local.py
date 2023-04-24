800afeaa02b483812ce7be902f1e851766e27a97
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-04-24 21:24:48 +0900

    ci: create .env file during workflow

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -6,10 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="Zum7ZvgmASVRgIRq65j8frn4uHFmq2KCxkLxFnhRuj6dc06aPj7kum9zga0vQlRy",
-)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
