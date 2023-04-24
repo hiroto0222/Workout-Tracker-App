a2e88b7c7a0ef30297ce8ef12c005b705477c89f
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-04-25 00:02:45 +0900

    feat (core): register Exercise model to admin

--- app/app/core/admin.py
+++ app/app/core/admin.py
@@ -1,3 +1,19 @@
+"""
+Admin configuration for core app
+"""
+
+from core.models import Exercise
 from django.contrib import admin
 
-# Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "body_part",
+        "name",
+        "equipment",
+        "description",
+    )
+
+
+admin.site.register(Exercise, ExerciseAdmin)
