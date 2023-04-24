70d272ada212daef39f77dc0cd0c9a7bf73f2e8b
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-04-24 23:35:15 +0900

    Revert "chore (lint): add pre-commit"
    This reverts commit 2ae7bc24fc3cefb5a8e384e5c0ca2d1f034d88fe.

--- app/app/core/models.py
+++ app/app/core/models.py
@@ -1,37 +1,3 @@
-"""
-Models for the core app
-"""
-
 from django.db import models
 
-
-class Exercise(models.Model):
-    """Exercise model
-
-    Attributes:
-        name (str): Name of the exercise
-        description (str): Description of the exercise
-    """
-
-    BODY_PARTS = (
-        ("Forearms", "Forearms"),
-        ("Triceps", "Triceps"),
-        ("Biceps", "Biceps"),
-        ("Neck", "Neck"),
-        ("Shoulders", "Shoulders"),
-        ("Chest", "Chest"),
-        ("Back", "Back"),
-        ("Core", "Core"),
-        ("Upper Legs", "Upper Legs"),
-        ("Glutes", "Glutes"),
-        ("Calves", "Calves"),
-        ("Full Body", "Full Body"),
-        ("Other", "Other"),
-    )
-
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    body_part = models.CharField(max_length=16, choices=BODY_PARTS)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
