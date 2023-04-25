dceaba80f99c4f7c4c88ea9ad0cd2a52574590ac
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-04-25 14:16:45 +0900

    feat: add Swagger API View for docs

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 
 from core.urls import core_router
@@ -25,6 +27,12 @@ router.registry.extend(core_router.registry)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
     path("api/", include((router.urls, "api"))),
 ]
 
